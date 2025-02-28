5e8ad281a3c18c2d0aaf5299acac7824c1c1b720
dimplesharma2195 <77977380+dimplesharma2195@users.noreply.github.com>
2025-02-28 12:00:09 +0530

    AuthContext.js

--- Assignments/Assignment_20/code/src/context/AuthContext.js
+++ Assignments/Assignment_20/code/src/context/AuthContext.js
@@ -1,9 +1,16 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [token, setToken] = useState(() => localStorage.getItem('idToken') || null);
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const storedToken = localStorage.getItem('idToken');
+    if (storedToken) {
+      setToken(storedToken);
+    }
+  }, []);
 
   const login = (idToken) => {
     setToken(idToken);
